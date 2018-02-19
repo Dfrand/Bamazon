670fafc07787281f3fa1bb5f8a852a2dc7f9ee31
Derek <d.frand@yahoo.com>
2018-02-19 10:47:23 -0600

    created/connected database

---
+++ Bamazon.sql
@@ -0,0 +1,13 @@
+create database bamazon;
+
+use bamazon;
+
+CREATE TABLE products
+(
+    item_id INTEGER AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(45) NOT NULL,
+    department_name VARCHAR(45) NOT NULL,
+    price DECIMAL(10 , 4 ) NOT NULL,
+    stock_quantity INTEGER(10) NOT NULL,
+    PRIMARY KEY (item_id)
+);
